7e3e1802a41175862af9e760650022892e93189a
brunoblock <brunoblock@oyster.ws>
2017-12-24 09:16:54 -0500

    Initial commit

---
+++ oy_hook.php
@@ -0,0 +1,15 @@
+<?php
+
+$oy_hook_payout = "0x00F72426ccE219B5Fa1b7E4C680F4440669Ba273";
+
+$oy_hook_permitted = array("broker1.oyster.ws");//can sign up for multiple broker nodes
+
+
+
+
+
+//validates connection with broker node
+
+//provides access to local iri api
+
+//provides ethereum address for PRL payment
