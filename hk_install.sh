48135b975b7209bc5791ddba728a3c9183df7306
automyr <35855008+automyr@users.noreply.github.com>
2018-01-27 01:58:48 +0100

    Adding a bash script to automate the hooknode install on a clean Ubuntu image

---
+++ hk_install.sh
@@ -0,0 +1,81 @@
+#!/bin/bash
+
+#install php and dependencies
+apt-get update
+apt-get install php
+apt-get install nginx
+apt-get install php-fpm
+apt-get install php-curl
+
+#install node (for Nelson)
+curl -sL https://deb.nodesource.com/setup_8.x | sudo -E bash -
+sudo apt-get install -y nodejs
+
+#install IRI
+sudo apt install software-properties-common -y && sudo add-apt-repository ppa:webupd8team/java -y && sudo apt update && sudo apt install oracle-java8-installer curl wget jq git -y && sudo apt install oracle-java8-set-default -y
+sudo sh -c 'echo JAVA_HOME="/usr/lib/jvm/java-8-oracle" >> /etc/environment' && source /etc/environment
+sudo useradd -s /usr/sbin/nologin -m iota
+sudo -u iota mkdir -p /home/iota/node /home/iota/node/ixi /home/iota/node/mainnetdb
+sudo -u iota wget -O /home/iota/node/iri-1.4.1.7.jar https://github.com/iotaledger/iri/releases/download/v1.4.1.7/iri-1.4.1.7.jar
+
+#find RAM, in MB
+phymem=$(awk -F":" '$1~/MemTotal/{print $2}' /proc/meminfo )
+phymem=${phymem:0:-2}
+phymem=$(($phymem/1000))
+xmx="Xmx"
+xmx_end="m"
+xmx=$xmx$phymem$xmx_end
+
+#set up Systemd service
+cat << "EOF" | sudo tee /lib/systemd/system/iota.service
+[Unit]
+Description=IOTA (IRI) full node
+After=network.target
+
+[Service]
+WorkingDirectory=/home/iota/node
+User=iota
+PrivateDevices=yes
+ProtectSystem=full
+Type=simple
+ExecReload=/bin/kill -HUP $MAINPID
+KillMode=mixed
+KillSignal=SIGTERM
+TimeoutStopSec=60
+ExecStart=/usr/bin/java -$xmx -Djava.net.preferIPv4Stack=true -jar iri-1.4.1.7.jar -c iota.ini
+SyslogIdentifier=IRI
+Restart=on-failure
+RestartSec=30
+
+[Install]
+WantedBy=multi-user.target
+Alias=iota.service
+
+EOF
+#configure IRI
+cat << "EOF" | sudo -u iota tee /home/iota/node/iota.ini
+[IRI]
+PORT = 14265
+UDP_RECEIVER_PORT = 14600
+TCP_RECEIVER_PORT = 15600
+API_HOST = 0.0.0.0
+IXI_DIR = ixi
+HEADLESS = true
+DEBUG = false
+TESTNET = false
+DB_PATH = mainnetdb
+RESCAN_DB = false
+
+REMOTE_LIMIT_API = "interruptAttachingToTangle, attachToTangle, setApiRateLimit"
+#We don't need to add normal neighbors as we're going to be using Nelson
+EOF
+#Download the last known Tangle database
+cd /tmp/ && curl -LO http://db.iota.partners/IOTA.partners-mainnetdb.tar.gz && sudo -u iota tar xzfv /tmp/IOTA.partners-mainnetdb.tar.gz -C /home/iota/node/mainnetdb && rm /tmp/IOTA.partners-mainnetdb.tar.gz
+#install Nelson
+npm install -g nelson.cli
+#start the IOTA service
+sudo service iota start
+#configure auto updates for IRI 
+echo '*/15 * * * * root bash -c "bash <(curl -s https://gist.githubusercontent.com/zoran/48482038deda9ce5898c00f78d42f801/raw)"' | sudo tee /etc/cron.d/iri_updater > /dev/null
+#Start Nelson
+nelson --gui --getNeighbors
