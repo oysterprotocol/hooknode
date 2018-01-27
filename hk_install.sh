e5e152ab934796e3df0c4a07d21ef3d7a06885e5
nymd <jafirmani@gmail.com>
2018-01-26 18:49:13 -0800

    Remove java agreement before installing

--- hk_install.sh
+++ hk_install.sh
@@ -12,6 +12,9 @@ curl -sL https://deb.nodesource.com/setup_8.x | sudo -E bash -
 sudo apt-get -y install -y nodejs
 
 #install IRI
+#pre-check Java agreements
+echo debconf shared/accepted-oracle-license-v1-1 select true | sudo debconf-set-selections
+echo debconf shared/accepted-oracle-license-v1-1 seen true | sudo debconf-set-selections
 sudo apt-get -y install software-properties-common -y && sudo add-apt-repository ppa:webupd8team/java -y && sudo apt update && sudo apt install oracle-java8-installer curl wget jq git -y && sudo apt install oracle-java8-set-default -y
 sudo sh -c 'echo JAVA_HOME="/usr/lib/jvm/java-8-oracle" >> /etc/environment' && source /etc/environment
 sudo useradd -s /usr/sbin/nologin -m iota
