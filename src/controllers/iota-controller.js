792d3b17c4347c292104cb032ae3c8f79f890974
Aaron Vasquez <aaronvasquez1@gmail.com>
2018-02-09 10:45:01 -0500

    debug

--- src/controllers/iota-controller.js
+++ src/controllers/iota-controller.js
@@ -2,7 +2,7 @@ import IOTA from "iota.lib.js";
 
 const CONFIG = Object.freeze({
   PROVIDER: new IOTA({ provider: "http://localhost:14265" }),
-  MIN_DEPTH: 1,
+  MIN_DEPTH: 3,
   MIN_WEIGHT_MAGNITUDE: 14,
 });
 
@@ -12,14 +12,24 @@ export const legacy = (req, res) => {
   const { trytes } = req.body;
   const { PROVIDER, MIN_DEPTH, MIN_WEIGHT_MAGNITUDE } = CONFIG;
 
-  const txs = PROVIDER.api.sendTrytes(
-    trytes,
-    MIN_DEPTH,
-    MIN_WEIGHT_MAGNITUDE,
-    console.log,
-  );
-  console.log(txs);
+  PROVIDER.api.sendTrytes(trytes, MIN_DEPTH, MIN_WEIGHT_MAGNITUDE, console.log);
 
   // Async response
   return res.status(204).send("success");
+};
+
+export const debug = (req, res) => {
+  console.log("testing123...");
+
+  const { PROVIDER } = CONFIG;
+  PROVIDER.api.findTransactionObjects(
+    {
+      addresses: [
+        "9BUABBUAPCRCVAZARCBBPCPCRCBBWAPCPCWAUCBBXAPCWAZA9BBBYASC9BQCXAXAWAQCWAYABBWAUCUAUDWZNUMLSB",
+      ],
+    },
+    console.log,
+  );
+
+  res.status(204).send("success");
 };
--- src/routes.js
+++ src/routes.js
@@ -2,6 +2,7 @@ import * as iotaController from "./controllers/iota-controller";
 
 export default app => {
   app.post("/HookListener.php", iotaController.legacy);
+  app.get("/debug", iotaController.debug);
 
   // TODO: Mirror IRI API.
 
