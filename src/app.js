400e65d7035e409c925197ca38666129906e9575
Aaron Vasquez <aaronvasquez1@gmail.com>
2018-02-09 16:31:35 -0500

    Environment vars

--- src/app.js
+++ src/app.js
@@ -1,10 +1,14 @@
 import bodyParser from "body-parser";
 import compression from "compression";
+import dotenv from "dotenv";
 import errorHandler from "errorhandler";
 import express from "express";
 import morgan from "morgan";
 
 import routes from "./routes";
+
+// Configure Environment
+dotenv.config();
 
 // Create Express server
 const app = express();
