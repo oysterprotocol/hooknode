69f5f88b28bb8888188b8c00afa04dc2ba10e4b6
Aaron Vasquez <aaronvasquez1@gmail.com>
2018-02-12 20:21:40 -0500

    WIP golang version of hooknode

--- main.go
+++ main.go
@@ -1,11 +1,16 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"log"
 	"net/http"
 	"os"
+	"reflect"
+	"runtime"
+	"strconv"
 
+	"github.com/iotaledger/giota"
 	"github.com/joho/godotenv"
 )
 
@@ -17,26 +22,69 @@ func main() {
 	}
 
 	// Attach handlers
-	http.HandleFunc("/", indexHandlers)
+	http.HandleFunc("/", indexHandler)
+	http.HandleFunc("/pow", powHandler)
 
 	// Fetch port from ENV
 	port := os.Getenv("PORT")
-	if port == "" {
-		port = "3000"
-	}
 	fmt.Print("Listing on http://localhost:" + port)
 
-	provider := os.Getenv("PROVIDER")
-	fmt.Print("Provider: " + provider)
-
 	// Start listening
-	err = http.ListenAndServe(":"+port, nil)
-	if err != nil {
-		log.Fatal("Could not start server: ", err)
-	}
+	http.ListenAndServe(":"+port, nil)
 }
 
-func indexHandlers(w http.ResponseWriter, r *http.Request) {
-	provider := os.Getenv("PROVIDER")
-	fmt.Fprintf(w, "PROVIDER %s", provider)
+func indexHandler(w http.ResponseWriter, r *http.Request) {
+	if r.Method == "POST" {
+		// Decode body
+		decoder := json.NewDecoder(r.Body)
+		var body map[string]interface{}
+		err := decoder.Decode(&body)
+		if err != nil {
+			panic(err)
+		}
+
+		// Unmarshal JSON to []giota.Transaction
+		trytesJSON, _ := body["trytes"].([][]byte)
+		txs := make([]giota.Transaction, len(trytesJSON))
+		for i, t := range trytesJSON {
+			var tx = giota.Transaction{}
+			tx.UnmarshalJSON(t)
+			txs[i] = tx
+		}
+
+		provider := os.Getenv("PROVIDER")
+		minDepth, _ := strconv.ParseInt(os.Getenv("MIN_DEPTH"), 10, 64)
+		minWeightMag, _ := strconv.ParseInt(os.Getenv("MIN_WEIGHT_MAGNITUDE"), 10, 64)
+
+		api := giota.NewAPI(provider, nil)
+		_, pow := giota.GetBestPoW()
+
+		// fmt.Printf("\n%v\n", txs, minDepth, minWeightMag, provider)
+
+		giota.SendTrytes(api, minDepth, txs, minWeightMag, pow)
+
+		w.WriteHeader(http.StatusNoContent)
+	} else {
+		http.Error(w, "Invalid request method", http.StatusMethodNotAllowed)
+	}
+
+}
+
+func powHandler(w http.ResponseWriter, r *http.Request) {
+	_, pow := giota.GetBestPoW()
+
+	// TODO: Figure out how to print the func name.
+	body, err :=
+		json.Marshal(map[string]interface{}{"powAlgo": getFuncName(pow)})
+
+	if err != nil {
+		http.Error(w, err.Error(), 500)
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	w.Write(body)
+}
+
+func getFuncName(i interface{}) string {
+	return runtime.FuncForPC(reflect.ValueOf(i).Pointer()).Name()
 }
