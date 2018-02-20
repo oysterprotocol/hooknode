4dd8f51c85b864ca9316e720c8ba30be26a86018
aerth <aerth@riseup.net>
2018-02-20 00:44:53 -0800

    get pow string, remove reflect and runtime packages

--- main.go
+++ main.go
@@ -7,8 +7,6 @@ import (
 	"log"
 	"net/http"
 	"os"
-	"reflect"
-	"runtime"
 	"strconv"
 
 	"github.com/getsentry/raven-go"
@@ -95,11 +93,9 @@ func indexHandler(w http.ResponseWriter, r *http.Request) {
 }
 
 func powHandler(w http.ResponseWriter, r *http.Request) {
-	_, pow := giota.GetBestPoW()
+	pow, _ := giota.GetBestPoW()
 
-	// TODO: Figure out how to print the func name.
-	body, err :=
-		json.Marshal(map[string]interface{}{"powAlgo": getFuncName(pow)})
+	body, err := json.Marshal(map[string]string{"powAlgo": pow})
 
 	if err != nil {
 		http.Error(w, err.Error(), 500)
@@ -107,10 +103,6 @@ func powHandler(w http.ResponseWriter, r *http.Request) {
 
 	w.Header().Set("Content-Type", "application/json")
 	w.Write(body)
-}
-
-func getFuncName(i interface{}) string {
-	return runtime.FuncForPC(reflect.ValueOf(i).Pointer()).Name()
 }
 
 func statsHandler(w http.ResponseWriter, r *http.Request) {
